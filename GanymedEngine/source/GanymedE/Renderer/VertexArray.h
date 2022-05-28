026f59b5c6194bcf54edfae4d0db79c9ed0fec9a
giska1923 <37376562+giska1923@users.noreply.github.com>
2022-05-28 20:58:49 +0200

    Vertex Buffer and Vertex Array
    Import Vertex Buffer and Vertex Array logics, that also abstract the use of them for each implementation. Create first implementation - OpenGL.

---
+++ GanymedEngine/source/GanymedE/Renderer/VertexArray.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+#include "GanymedE/Renderer/Buffer.h"
+
+namespace GanymedE {
+	class VertexArray
+	{
+	public:
+		virtual ~VertexArray() {}
+
+		virtual void Bind() const = 0;
+		virtual void Unbind() const = 0;
+
+		virtual void AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer) = 0;
+		virtual void SetIndexBuffer(const std::shared_ptr<IndexBuffer>& vertexBuffer) = 0;
+
+		virtual const std::vector<std::shared_ptr<VertexBuffer>>& GetVertexBuffers() const = 0;
+		virtual const std::shared_ptr<IndexBuffer>& GetIndexBuffer() const = 0;
+
+		static VertexArray* Create();
+	};
+}
--- GanymedEngine/source/GanymedE/main/Application.h
+++ GanymedEngine/source/GanymedE/main/Application.h
@@ -10,6 +10,7 @@
 
 #include "GanymedE/Renderer/Shader.h"
 #include "GanymedE/Renderer/Buffer.h"
+#include "GanymedE/Renderer/VertexArray.h"
 
 namespace GanymedE {
 	class WindowCloseEvent;
@@ -36,10 +37,11 @@ namespace GanymedE {
 
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 	private:
 		static Application* s_instance;
 	};
