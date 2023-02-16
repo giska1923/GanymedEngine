acb5437f4b5465b0f5183eece1b601ecc323e22d
giska1923 <37376562+giska1923@users.noreply.github.com>
2023-02-16 13:52:36 +0100

    Shader abstraction
    Abstract shader class and add OpenGLShader (for now). Also transform the rendered objects and add ImGui option to dynamically change color of the rendering squares.

--- Sandbox/source/SandboxApp.cpp
+++ Sandbox/source/SandboxApp.cpp
@@ -1,6 +1,11 @@
 #include <GanymedE.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer :public GanymedE::Layer {
 public:
@@ -33,10 +38,10 @@ public:
 		m_SquareVA.reset(GanymedE::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<GanymedE::VertexBuffer> squareVB;
@@ -58,6 +63,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -66,7 +72,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}		
 		)";
 
@@ -85,38 +91,41 @@ public:
 			}		
 		)";
 
-		m_Shader.reset(new GanymedE::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(GanymedE::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}		
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}		
 		)";
 
-		m_BlueShader.reset(new GanymedE::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(GanymedE::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(GanymedE::Timestep ts) override
@@ -156,7 +165,21 @@ public:
 
 		GanymedE::Renderer::BeginScene(m_Camera);
 
-		GanymedE::Renderer::Submit(m_BlueShader, m_SquareVA);
+		static glm::mat4 scale = glm::scale(glm::mat4(1.f), glm::vec3(0.1f));
+
+		std::dynamic_pointer_cast<GanymedE::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<GanymedE::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.f), pos) * scale;
+				GanymedE::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
+			}
+		}
+		
 		GanymedE::Renderer::Submit(m_Shader, m_VertexArray);
 
 		GanymedE::Renderer::EndScene();
@@ -164,7 +187,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(GanymedE::Event& event) override
@@ -176,7 +201,7 @@ private:
 	std::shared_ptr<GanymedE::Shader> m_Shader;
 	std::shared_ptr<GanymedE::VertexArray> m_VertexArray;
 
-	std::shared_ptr<GanymedE::Shader> m_BlueShader;
+	std::shared_ptr<GanymedE::Shader> m_FlatColorShader;
 	std::shared_ptr<GanymedE::VertexArray> m_SquareVA;
 
 	GanymedE::OrthographicCamera m_Camera;
@@ -185,6 +210,8 @@ private:
 
 	float m_CameraRotation = 0.f;
 	float m_CameraRotationSpeed = 180.f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 
