a3de2525674e008b83334dc5159664d002dd101d
giska1923 <37376562+giska1923@users.noreply.github.com>
2023-03-16 23:22:35 +0100

    Shaders can now be read from files
    Added support for reading .glsl files and automatically compile and link shaders while reading it from files. Also, added ShaderLibrary for storing multiple shaders by name.

--- Sandbox/source/SandboxApp.cpp
+++ Sandbox/source/SandboxApp.cpp
@@ -57,111 +57,11 @@ public:
 		squareIB.reset(GanymedE::IndexBuffer::Create(squareIndices, std::size(squareIndices)));
 		m_SquareVA->SetIndexBuffer(squareIB);
 
-		std::string vertexSrc = R"(
-			#version 330 core
+		m_Shader = GanymedE::Shader::Create("assets/shaders/VertexPosColor.glsl");
 
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec4 a_Color;
+		m_FlatColorShader = GanymedE::Shader::Create("assets/shaders/FlatColor.glsl");
 
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-
-			out vec3 v_Position;
-			out vec4 v_Color;
-
-			void main()
-			{
-				v_Position = a_Position;
-				v_Color = a_Color;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-			}		
-		)";
-
-		std::string fragmentSrc = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-			in vec4 v_Color;
-
-			void main()
-			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
-				color = v_Color;
-			}		
-		)";
-
-		m_Shader.reset(GanymedE::Shader::Create(vertexSrc, fragmentSrc));
-
-		std::string flatColorShaderVertexSrc = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;
-
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-
-			out vec3 v_Position;
-
-			void main()
-			{
-				v_Position = a_Position;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-			}		
-		)";
-
-		std::string flatColorShaderFragmentSrc = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-
-			uniform vec3 u_Color;
-
-			void main()
-			{
-				color = vec4(u_Color, 1.0);
-			}		
-		)";
-
-		m_FlatColorShader.reset(GanymedE::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
-
-		std::string textureShaderVertexSrc = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TexCoord;
-
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-
-			out vec2 v_TexCoord;
-
-			void main()
-			{
-				v_TexCoord = a_TexCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-			}		
-		)";
-
-		std::string textureShaderFragmentSrc = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;
-
-			in vec2 v_TexCoord;
-
-			uniform sampler2D u_Texture;
-
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}		
-		)";
-
-		m_TextureShader.reset(GanymedE::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+		m_TextureShader = GanymedE::Shader::Create("assets/shaders/Texture.glsl");
 
 		m_Texture = GanymedE::Texture2D::Create("assets/textures/Checkerboard.png");
 
@@ -243,6 +143,7 @@ public:
 	}
 
 private:
+	GanymedE::ShaderLibrary m_ShaderLibrary;
 	GanymedE::Ref<GanymedE::Shader> m_Shader;
 	GanymedE::Ref<GanymedE::VertexArray> m_VertexArray;
 
